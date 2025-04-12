80e938a661b464ab2e9761098f9b799cc2b3a29a
Innsbluck <innsbluck@gmail.com>
2025-04-12 20:53:40 +0900

    support mouse zoom

--- src/components/canvas/CanvasArea.tsx
+++ src/components/canvas/CanvasArea.tsx
@@ -5,7 +5,7 @@ import { canvasStore, metricStore, setMetricStore } from "~/stores/Store";
 
 import styles from "./canvas_area.module.css"
 import Controls from "./controls/Controls";
-import { onMount } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 
 export default () => {
     let canvasStackRef: HTMLDivElement;
@@ -19,6 +19,15 @@ export default () => {
         })
     })
 
+
+
+    function addOffset(x: number, y: number) {
+        setMetricStore("offset", {
+            x: metricStore.offset.x + x,
+            y: metricStore.offset.y + y,
+        });
+    }
+
     function setupZoomPan() {
         let lastX: number[] = [0, 0];
         let lastY: number[] = [0, 0];
@@ -30,19 +39,11 @@ export default () => {
             if (e.touches.length === 1) {
                 const xMove0 = e.touches[0].clientX - lastX[0];
                 if (xMove0 !== 0 && lastX[0] !== 0) {
-                    console.log(`move X: ${xMove0} => ${metricStore.offset.x + xMove0}`)
-                    setMetricStore("offset", {
-                        x: metricStore.offset.x + xMove0,
-                        y: metricStore.offset.y,
-                    })
+                    addOffset(xMove0, 0);
                 }
                 const yMove0 = e.touches[0].clientY - lastY[0];
                 if (yMove0 !== 0 && lastY[0] !== 0) {
-                    console.log(`move Y: ${yMove0} => ${metricStore.offset.y + yMove0}`)
-                    setMetricStore("offset", {
-                        x: metricStore.offset.x,
-                        y: metricStore.offset.y + yMove0,
-                    })
+                    addOffset(0, yMove0);
                 }
                 lastX[0] = e.touches[0].clientX;
                 lastY[0] = e.touches[0].clientY;
@@ -50,7 +51,7 @@ export default () => {
             if (e.touches.length === 2) {
                 const dx = e.touches[0].clientX - e.touches[1].clientX;
                 const dy = e.touches[0].clientY - e.touches[1].clientY;
-                const dist = Math.sqrt(dx * dx + dy * dy) * metricStore.zoomSensitivity;
+                const dist = Math.sqrt(dx * dx + dy * dy) * metricStore.touchZoomSensitivity;
                 if (lastDist !== 0) {
                     const scaleFactor = dist / lastDist;
                     const zoomOld = metricStore.zoom;
@@ -70,21 +71,13 @@ export default () => {
                 const xMove1 = e.touches[1].clientX - lastX[1];
                 const mutualMoveX = getMutualMove(xMove0, xMove1);
                 if (mutualMoveX !== 0 && lastX[0] !== 0 && lastX[1] !== 0) {
-                    console.log(`move X: ${mutualMoveX} => ${metricStore.offset.x + mutualMoveX}`)
-                    setMetricStore("offset", {
-                        x: metricStore.offset.x + mutualMoveX,
-                        y: metricStore.offset.y,
-                    })
+                    addOffset(mutualMoveX, 0);
                 }
                 const yMove0 = e.touches[0].clientY - lastY[0];
                 const yMove1 = e.touches[1].clientY - lastY[1];
                 const mutualMoveY = getMutualMove(yMove0, yMove1);
                 if (mutualMoveY !== 0 && lastY[0] !== 0 && lastY[1] !== 0) {
-                    console.log(`move Y: ${mutualMoveY} => ${metricStore.offset.y + mutualMoveY}`)
-                    setMetricStore("offset", {
-                        x: metricStore.offset.x,
-                        y: metricStore.offset.y + mutualMoveY,
-                    })
+                    addOffset(0, mutualMoveY);
                 }
                 lastX[0] = e.touches[0].clientX;
                 lastX[1] = e.touches[1].clientX;
@@ -99,6 +92,64 @@ export default () => {
             lastY = [0, 0];
             lastDist = 0;
         });
+
+        wrapper.addEventListener("wheel", (e) => {
+            e.preventDefault();
+            const delta = e.deltaY > 0 ? -metricStore.wheelZoomStep : metricStore.wheelZoomStep;
+
+            const zoomOld = metricStore.zoom;
+            const zoomNew = Math.max(0.1, Math.min(8, metricStore.zoom + delta));
+            const rect = canvasStackRef.getBoundingClientRect();
+            const canvasX = (e.clientX - rect.left) / zoomOld;
+            const canvasY = (e.clientY - rect.top) / zoomOld;
+            setMetricStore("zoom", zoomNew);
+            setMetricStore("offset", {
+                x: metricStore.offset.x + canvasX * (zoomOld - zoomNew),
+                y: metricStore.offset.y + canvasY * (zoomOld - zoomNew),
+            });
+        });
+
+        const [isDrag, setIsDrag] = createSignal(false);
+        const [dragPosition, setDragPosition] = createSignal({ x: 0, y: 0 });
+        const [isCtrlPressed, setCtrlPressed] = createSignal(false);
+
+        window.addEventListener("keydown", (e) => {
+            if (e.key === "Control") setCtrlPressed(true);
+        })
+
+        window.addEventListener("keyup", (e) => {
+            if (e.key === "Control") setCtrlPressed(false);
+        })
+
+        wrapper.addEventListener("mousedown", (e) => {
+            if (isCtrlPressed()) {
+                e.preventDefault();
+                e.stopPropagation();
+                setIsDrag(true);
+                setDragPosition({ x: e.clientX, y: e.clientY });
+            }
+        })
+
+        wrapper.addEventListener("mousemove", (e) => {
+            if (isCtrlPressed() && isDrag()) {
+                e.preventDefault();
+                e.stopPropagation();
+                const dx = e.clientX - dragPosition().x;
+                const dy = e.clientY - dragPosition().y;
+                addOffset(dx, dy)
+                setDragPosition({ x: e.clientX, y: e.clientY });
+            }
+        })
+
+        wrapper.addEventListener("mouseup", (e) => {
+            setIsDrag(false);
+        })
+        wrapper.addEventListener("mouseleave", (e) => {
+            setIsDrag(false);
+        })
+        wrapper.addEventListener("mouseout", (e) => {
+            setIsDrag(false);
+        })
     }
 
     const offsetX = () => metricStore.offsetOrigin.x + metricStore.offset.x;
@@ -125,13 +176,13 @@ export default () => {
                     width: "100%",
                     height: "100%",
                     "touch-action": "none",
-                    "transform-origin": "0 0",
-                    transform: `translate(${offsetX()}px, ${offsetY()}px) scale(${zoom()})`,
                 }}
             >
                 <div ref={(el) => canvasStackRef = el} style={{
                     padding: 0,
                     margin: 0,
+                    "transform-origin": "0 0",
+                    transform: `translate(${offsetX()}px, ${offsetY()}px) scale(${zoom()})`,
                 }}>
                     <CanvasStack />
                 </div>
@@ -141,6 +192,7 @@ export default () => {
         </div>
     );
 }
+
 const getMutualMove = (move0: number, move1: number) => {
     // 逆方向なら0
     if (Math.sign(move0) !== Math.sign(move1)) return 0;
--- src/stores/Store.ts
+++ src/stores/Store.ts
@@ -17,7 +17,8 @@ export const [metricStore, setMetricStore] = createStore({
   lastMouseOnCanvas: { x: 0, y: 0 },
   isInStroke: false,
   zoom: 1,
-  zoomSensitivity: 0.5,
+  touchZoomSensitivity: 0.5,
+  wheelZoomStep: 0.1,
   // オフセットの初期値
   offsetOrigin: { x: 0, y: 0 },
   offset: { x: 0, y: 0 },

7e43dc24742ae34feb1231a387383d188d73737b
Innsbluck <innsbluck@gmail.com>
2025-04-29 17:29:56 +0900

    keyconfig commands

---
+++ src/controllers/history/HistoryController.ts
@@ -0,0 +1,29 @@
+import { layerAgentManager } from '~/routes/editor';
+
+export function undoLayer(layerId: string) {
+  const agent = layerAgentManager.getAgent(layerId);
+  if (!agent) {
+    console.log(`no agent found for  ${layerId}.`);
+    return;
+  }
+
+  if (agent.canUndo()) {
+    console.log(`undo layer ${layerId}.`);
+    agent.undo();
+  } else {
+    console.log(`can't undo layer ${layerId}.`);
+  }
+}
+
+export function redoLayer(layerId: string) {
+  const agent = layerAgentManager.getAgent(layerId);
+  if (!agent) {
+    return;
+  }
+
+  if (agent.canRedo()) {
+    agent.redo();
+  } else {
+    console.log(`can't redo layer ${layerId}.`);
+  }
+}
--- src/controllers/tool/ToolController.ts
+++ src/controllers/tool/ToolController.ts
@@ -1,3 +1,13 @@
-import { toolStore } from '~/stores/EditorStores';
+import { setToolStore, toolStore } from '~/stores/EditorStores';
+import { ToolType } from '~/types/Tool';
 
 export const currentTool = () => toolStore.tools[toolStore.usingIndex];
+
+export function switchToolType(type: ToolType) {
+  const indexToSwitch = toolStore.tools.findIndex((tool) => tool.type === type);
+  switchTool(indexToSwitch);
+}
+
+export function switchTool(index: number) {
+  setToolStore('usingIndex', index);
+}

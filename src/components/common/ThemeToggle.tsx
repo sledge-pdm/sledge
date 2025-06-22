11092014043e01b4b76ead4c03e099846748bb3e
Innsbluck <innsbluck@gmail.com>
2025-06-22 22:17:12 +0900

    fps etc config fixes++

--- src/components/common/ThemeToggle.tsx
+++ src/components/common/ThemeToggle.tsx
@@ -2,7 +2,7 @@ import { Theme } from '@tauri-apps/api/window';
 import { Component } from 'solid-js';
 import Dropdown from '~/components/common/control/Dropdown';
 import { saveGlobalSettings } from '~/io/config/out/save';
-import { themeOptions } from '~/models/config/GlobalConfig';
+import { themeOptions } from '~/models/config/types/Theme';
 import { globalConfig, setGlobalConfig } from '~/stores/GlobalStores';
 
 const ThemeToggle: Component<{ noBackground?: boolean }> = (props) => {
---
+++ src/models/config/Sections.ts
@@ -0,0 +1,8 @@
+export enum Sections {
+  General = 'GENERAL',
+  Editor = 'EDITOR',
+  Performance = 'PERFORMANCE',
+  ProjectDefaults = 'DEFAULTS',
+  KeyConfig = 'KEY CONFIG',
+  Debug = 'DEBUG',
+}

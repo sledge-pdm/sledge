35da6d156001373b42dc5cae1163e71696691be0
Innsbluck <innsbluck@gmail.com>
2025-07-09 19:30:42 +0900

    push monorepo!

---
+++ packages/core/src/snippets.css.ts
@@ -0,0 +1,21 @@
+import { style } from '@vanilla-extract/css';
+
+export const flexCol = style({
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+export const flexRow = style({
+  display: 'flex',
+  flexDirection: 'row',
+});
+
+export const w100 = style({
+  width: '100%',
+});
+
+export const h100 = style({
+  height: '100%',
+});
+
+export const wh100 = style([w100, h100]);

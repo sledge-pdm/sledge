aa0b8bcc2513483d48fb0edc134a7192293f40ff
Innsbluck <innsbluck@gmail.com>
2025-08-03 00:36:55 +0900

    fix

--- apps/sledge/src/styles/components/canvas/canvas_area.css.ts
+++ apps/sledge/src/styles/components/canvas/canvas_area.css.ts
@@ -7,9 +7,6 @@ export const canvasArea = style([
   {
     backgroundColor: vars.color.canvasArea,
     display: 'flex',
-    position: 'relative',
-    height: '100%',
-    width: '100%',
     overflow: 'hidden',
     margin: 0,
     padding: 0,

ab833ecfcd5f512be1bb3d75aff12a27376b3f50
Innsbluck <innsbluck@gmail.com>
2025-08-04 14:21:04 +0900

    impl website downloads+++

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,10 +1,16 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import solidPlugin from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), solidPlugin(), tsconfigPaths()],
+  plugins: [
+    vanillaExtractPlugin(),
+    solidPlugin(),
+    tsconfigPaths(),
+    nodePolyfills({ include: ['process'], globals: { global: true, process: true } }),
+  ],
   server: {
     port: 3000,
   },

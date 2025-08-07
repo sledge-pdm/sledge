bec241b0c92df327be047eb5b86fb429c7da1746
Innsbluck <innsbluck@gmail.com>
2025-08-07 16:38:24 +0900

    macos

--- apps/sledge/src-tauri/src/window.rs
+++ apps/sledge/src-tauri/src/window.rs
@@ -142,12 +142,35 @@ pub async fn open_window(
         builder = builder.decorations(false);
     }
 
+    #[cfg(target_os = "macos")]
+    let builder = builder.title_bar_style(TitleBarStyle::Transparent);
+
     // 4. ウィンドウ生成（非表示で）
     #[allow(unused_variables)]
     let window = builder
         // .visible(false) // App.tsx側でshowするまでは非表示
         .build()
         .map_err(|e| e.to_string())?;
+
+        
+      // set background color only when building for macOS
+    #[cfg(target_os = "macos")]
+    {
+      use cocoa::appkit::{NSColor, NSWindow};
+      use cocoa::base::{id, nil};
+
+      let ns_window = window.ns_window().unwrap() as id;
+      unsafe {
+        let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+            nil,
+            50.0 / 255.0,
+            158.0 / 255.0,
+            163.5 / 255.0,
+            1.0,
+        );
+        ns_window.setBackgroundColor_(bg_color);
+      }
+    }
 
     Ok(())
 }

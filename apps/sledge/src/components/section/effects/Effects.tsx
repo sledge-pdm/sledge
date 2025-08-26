adb56ba86c1d7cefeec0f28a3ccc30f9f6469343
Innsbluck <innsbluck@gmail.com>
2025-08-26 20:12:03 +0900

    add info for effect

--- apps/sledge/src/components/section/effects/Effects.tsx
+++ apps/sledge/src/components/section/effects/Effects.tsx
@@ -1,11 +1,16 @@
+import { vars } from '@sledge/theme';
 import { Component } from 'solid-js';
 import GaussianBlur from '~/components/section/effects/items/GaussianBlur';
 import GrayScale from '~/components/section/effects/items/GrayScale';
 import Invert from '~/components/section/effects/items/Invert';
+import { activeLayer } from '~/controllers/layer/LayerListController';
 
 const Effects: Component = () => {
   return (
     <>
+      <p>
+        Effect will be applied to <span style={{ color: vars.color.active }}>{activeLayer().name}</span>.
+      </p>
       <Invert />
       <GrayScale />
       <GaussianBlur />

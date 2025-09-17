6d43f023bf5516b69b6bda2ac64a8254171275a9
Innsbluck <innsbluck@gmail.com>
2025-09-17 19:08:42 +0900

    enable right click erase++

--- apps/sledge/src/tools/ToolBehavior.ts
+++ apps/sledge/src/tools/ToolBehavior.ts
@@ -11,6 +11,7 @@ export interface ToolResult {
 
 export interface ToolBehavior {
   acceptStartOnOutCanvas?: boolean;
+  allowRightClick?: boolean;
   onlyOnCanvas?: boolean;
   isInstantTool?: boolean;
 

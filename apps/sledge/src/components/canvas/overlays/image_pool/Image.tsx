3c62c9c97c2fdf84c29f2e5989284f60e2042c7b
Innsbluck <innsbluck@gmail.com>
2025-10-17 00:52:52 +0900

    set imagepool image pixelated

--- apps/sledge/src/components/canvas/overlays/image_pool/Image.tsx
+++ apps/sledge/src/components/canvas/overlays/image_pool/Image.tsx
@@ -1,3 +1,4 @@
+import { css } from '@acab/ecsstatic';
 import { MenuListOption, showContextMenu } from '@sledge/ui';
 import { convertFileSrc } from '@tauri-apps/api/core';
 import { Component, createMemo, onMount } from 'solid-js';
@@ -8,6 +9,15 @@ import { getEntry, hideEntry, ImagePoolEntry, removeEntry, selectEntry, showEntr
 import { interactStore } from '~/stores/EditorStores';
 import { imagePoolStore } from '~/stores/ProjectStores';
 import { eventBus } from '~/utils/EventBus';
+
+const imageElement = css`
+  margin: 0;
+  padding: 0;
+  pointer-events: none;
+  touch-action: none;
+  z-index: var(--zindex-image-pool-image);
+  image-rendering: pixelated;
+`;
 
 const Image: Component<{ entry: ImagePoolEntry; index: number }> = (props) => {
   const [stateStore, setStateStore] = createStore({
@@ -180,17 +190,13 @@ const Image: Component<{ entry: ImagePoolEntry; index: number }> = (props) => {
         <img
           ref={(el) => (imageRef = el)}
           src={convertFileSrc(props.entry.originalPath)}
+          class={imageElement}
           width={stateStore.baseW}
           height={stateStore.baseH}
           style={{
-            margin: 0,
-            padding: 0,
             width: `${stateStore.baseW}px`,
             height: `${stateStore.baseH}px`,
-            'z-index': 'var(--zindex-image-pool-image)',
             opacity: stateStore.visible ? 1 : selected() ? 0.5 : 0,
-            'pointer-events': 'none',
-            'touch-action': 'none',
           }}
         />
       </div>

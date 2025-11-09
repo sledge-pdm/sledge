1b9d2aef090e752a016edaf1f2df0c51cbd709c2
Innsbluck <innsbluck@gmail.com>
2025-11-09 10:13:32 +0900

    color history

--- apps/sledge/src/features/color/service.ts
+++ apps/sledge/src/features/color/service.ts
@@ -1,5 +1,5 @@
 // Side-effectful operations / state interactions for color feature
-import { hexToRGBA } from '~/features/color';
+import { hexToRGBA, RGBAColor } from '~/features/color';
 import { projectHistoryController } from '~/features/history';
 import { ColorHistoryAction } from '~/features/history/actions/ColorHistoryAction';
 import { colorStore, setColorStore } from '~/stores/EditorStores';
@@ -38,6 +38,34 @@ export const selectPalette = (palette: PaletteType) => {
   return setColorStore('currentPalette', palette);
 };
 
+export const getCurrentSwatch = () => {
+  return colorStore.swatches.find((swatch) => swatch.name === colorStore.currentSwatchName);
+};
 export const setCurrentSwatch = (swatchName: string) => {
   return setColorStore('currentSwatchName', swatchName);
+};
+
+export const getColorHistory = () => {
+  return colorStore.history;
+};
+export const addColorHistory = (color: RGBAColor) => {
+  setColorStore('history', (old) => {
+    return [color, ...old].slice(0, 50);
+  });
+};
+
+export const registerColorChange = (oldColor: RGBAColor, newColor: RGBAColor) => {
+  // add project history
+  const action = new ColorHistoryAction({
+    palette: colorStore.currentPalette,
+    oldColor,
+    newColor,
+    context: {
+      from: 'registerColorChange',
+    },
+  });
+  projectHistoryController.addAction(action);
+
+  // add color history
+  addColorHistory(newColor);
 };

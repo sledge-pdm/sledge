691fc371bad226b9e434520aca790f258ac549a2
Innsbluck <innsbluck@gmail.com>
2026-01-23 13:01:30 +0900

    fix check update conditions

--- src/components/global/title_bar/UpdateSection.tsx
+++ src/components/global/title_bar/UpdateSection.tsx
@@ -1,5 +1,6 @@
 import { color } from '@sledge-pdm/ui';
 import { Component, createEffect, createSignal, onMount, Show } from 'solid-js';
+import { ioStore } from '~/stores/EditorStores';
 import { globalConfig } from '~/stores/GlobalStores';
 import { Update } from '~/utils/platform';
 import { askAndInstallUpdate, getUpdate } from '~/utils/UpdateUtils';
@@ -10,15 +11,24 @@ export const UpdateSection: Component = () => {
   const [availableUpdate, setAvailableUpdate] = createSignal<Update | undefined>();
 
   onMount(async () => {
-    const update = await getUpdate();
-    if (!availableUpdate()) setAvailableUpdate(update);
+    await checkUpdate();
   });
 
   createEffect(async () => {
     globalConfig.debug.updateChannel;
+    await checkUpdate();
+  });
+
+  createEffect(async () => {
+    if (ioStore.isInInitialLoading) {
+      await checkUpdate();
+    }
+  });
+  
+  const checkUpdate = async () => {
     const update = await getUpdate();
     if (!availableUpdate()) setAvailableUpdate(update);
-  });
+  };
 
   return (
     <>
